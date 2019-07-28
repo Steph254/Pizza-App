49a835b96328e535266e572d03ff638d2034009f
Stephania Mwikali <steph.mwikali@gmail.com>
2019-07-28 22:40:07 +0300

    Change alert message and also change price for topping on each size of pizza

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var orders: [];
+var orders=[];
 
 var pizzaSize = {
   small: 250,
@@ -11,11 +11,25 @@ var pizzaCrust = {
   thin: 0,
   glutenfree: 200
 }
-var pizzaTopping = {
-  mushroom: 50,
-  pepperoni: 50,
-  bacon: 50,
-  cheese: 0
+var pizzaToppings = {
+  small: {
+    mushroom: 50,
+    pepperoni: 50,
+    bacon: 50,
+    cheese: 0
+  },
+  medium: {
+    mushroom: 70,
+    pepperoni: 70,
+    bacon: 70,
+    cheese: 20
+  },
+  large: {
+    mushroom: 90,
+    pepperoni: 90,
+    bacon: 900,
+    cheese: 20
+  },
 }
 function Pizza(size, topping, crust) {
   this.size = size;
@@ -30,44 +44,66 @@ Pizza.prototype.calculateCost = function() {
   var crust = pizzaCrust[this.crust];
   var topping = 0;
   if ($('#toppingBacon').is(":checked")) {
-    topping += pizzaToppings['bacon'];
-  } else if ($('#toppingPepperoni').is(":checked")) {
-    topping += pizzaToppings['pepperoni'];
-  } else if ($('#toppingMushrooms').is(":checked")) {
-    topping += pizzaToppings['mushrooms'];
-  } else($('#toppingCheese').is(":checked")) {
-    topping += pizzaToppings['cheese'];
+    topping += parseInt(pizzaToppings[this.size]['bacon']);
+  }
+  if ($('#toppingPepperoni').is(":checked")) {
+    topping += parseInt(pizzaToppings[this.size]['pepperoni']);
+  }
+  if ($('#toppingMushrooms').is(":checked")) {
+    // console.log(size, crust, topping);
+    topping += parseInt(pizzaToppings[this.size]['mushroom']);
+  }
+  if ($('#toppingCheese').is(":checked")) {
+    // console.log(size, crust, topping);
+    topping += parseInt(pizzaToppings[this.size]['cheese']);
   }
 
+  console.log(size, crust, topping);
   return size + crust + topping;
 }
 
 var checkout = function(){
-
+    if($('#delivery').is(":checked")){
+      orders.push(150)
+    }
+    var total=0
+    for (var i = 0; i < orders.length; i++) {
+      total += parseInt(orders[i]);
+    }
+    alert("Total cost is"+" Ksh."+total);
 }
 
 $(document).ready(function() {
-  $("form#topping1").submit(function(event) {
-    event.preventDefault();
+  $("#checkout").click(function(){
+    checkout();
+  });
 
-    var inputtedSize = $("select#size").val();
-    var inputtedCrust = $("select#crust").val();
+
+  $("#topping1").submit(function(event) {
+    event.preventDefault();
+    var inputtedSize = $("#size").val();
+    var inputtedCrust = $("#crust").val();
+
     var inputtedTopping = '';
     if ($('#toppingBacon').is(":checked")) {
       inputtedTopping += ' Bacon';
-    } else if ($('#toppingMushrooms').is(":checked")) {
+    }
+    if ($('#toppingMushrooms').is(":checked")) {
       inputtedTopping += ' Mushrooms';
-    } else if ($('#toppingPepperoni').is(":checked")) {
+    }
+     if ($('#toppingPepperoni').is(":checked")) {
       inputtedTopping += ' Pepperoni';
-    } else($('#toppingCheese').is(":checked")) {
+    }
+     if($('#toppingCheese').is(":checked")) {
+      console.log(inputtedTopping)
       inputtedTopping += ' Cheese';
     }
 
     var newPizza = new Pizza(inputtedSize,inputtedTopping, inputtedCrust);
 
     $("ul#pizzaorder").append("<li><span class='order'>" + newPizza.fullOrder() + "</span></li>");
-      orders.push(newPizza.calculateCost());
+    orders.push(newPizza.calculateCost());
     $('input:checkbox:checked').prop('checked', false);
+    // @todo How to clear select
    });
   });
-});
